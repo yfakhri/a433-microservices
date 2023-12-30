f437146bf43cb51914d825df49d74f00705b5fcf
Yusuf Fakhri <y.fakhri12@gmail.com>
2023-12-30 13:05:57 +0700

    mod: circleci config andd add build push image shell command

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#membuat docker image menggunakan Dockerfile yang ada di folder ini dengan nama karsajobs dengan tag latest
+docker build -t ghcr.io/yfakhri/karsajobs:latest .
+
+#login ke github package container registry
+echo $GITHUB_TOKEN | docker login ghcr.io -u yfakhri --password-stdin
+
+#push image ke github package container registry
+docker push ghcr.io/yfakhri/karsajobs:latest
+
